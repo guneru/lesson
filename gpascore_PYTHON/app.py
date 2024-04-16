71588beba058564e89a57538ff8ae85ddc42b695
구인모 <ttrr7eek@naver.com>
2024-04-16 15:00:59 +0900

    x데이터, y데이터

--- gpascore_PYTHON/app.py
+++ gpascore_PYTHON/app.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 data = pd.read_csv('gpascore.csv')
-print(data)
+
+# print(data.isnull().sum())
+data = data.dropna()
+
+y데이터 = data['admit'].values
+
+x데이터 = [ ]
+
+for i, rows in data.iterrows():
+    x데이터.append([ rows['gre'], rows['gpa'], rows['rank'] ])
 
 import tensorflow as tf
 model = tf.keras.models.sequential([
@@ -12,4 +21,4 @@ model = tf.keras.models.sequential([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'] )
 
-# model.fit( x데이터, y데이터, epochs=10 )
+model.fit( x데이터, y데이터, epochs=10 )
